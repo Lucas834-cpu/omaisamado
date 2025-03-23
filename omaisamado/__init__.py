f8edb3791363692d687c528e50edb37a790aa778
Lucas834-cpu <lucasbr2301@outlook.com>
2025-03-23 15:28:24 -0300

    Update __init__.py

--- omaisamado/__init__.py
+++ omaisamado/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_required, login_user
+import os
 
 app = Flask(__name__)
 #deoretor é uma função, que atribui a outra função uma nova funcionalidade
@@ -10,7 +11,11 @@ app = Flask(__name__)
 #o arquivo "base.html" é um arquivo que será integrado com todas as páginas "html" para que todas tenham a mesma "cara"
 
 app.config['SECRET_KEY']='b5aba2e104289f3ca1b9ce0881671015'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.bd'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.bd'
+
 bcrypt = Bcrypt(app)
 login_menager = LoginManager(app)
 login_menager.login_view = 'login_criarconta'
